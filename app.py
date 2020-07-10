5644d65687190436adcfdd86282b7f780cd26e2c
WilsonSanch <wilson.sanchez8303@gmail.com>
2020-07-10 15:22:46 -0500

    Home ApiFlask

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 app.config['MONGO_URI']='mongodb+srv://Okan:Wil830305*@cluster0-ryris.mongodb.net/adnmeli?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
+@app.route('/')
+def home():
+    return 'Api ADN MELI Challenge'
+
+
 @app.route('/mutant', methods=['POST'])
 def ismutant():
     adn = request.json['adn']
